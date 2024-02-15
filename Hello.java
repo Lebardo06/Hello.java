394ff5eb6d2ae60bc499623d60b25ba271f21f4a
Edoardo Lebosi <edoardolebosi@MBP-di-Edoardo.station>
2024-02-15 17:30:47 +0100

    first commit

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int somma = a + b;
+        System.out.println("La somma di " + a + " e " + b + " è :" + somma);
+    }
+}
